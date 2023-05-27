0fc51aaba960e75325475c8f317fb4a95fde4400
naramos00 <125921189+Naramos00@users.noreply.github.com>
2023-05-26 20:18:48 -0700

    Complete rock, paper, scissors game.

--- rps.js
+++ rps.js
@@ -1,34 +1,60 @@
 function playRound(playerSelection, computerSelection){
     playerSelection = capitalize(playerSelection);
-
+    let winLoseMsg;
     if(playerSelection === "Rock" && computerSelection === "Scissors"){
-        comsole.log("You Win! Rock beats Scissors");
+        return winLoseMsg = "You Win! Rock beats Scissors";
     }
     else if(playerSelection === "Rock" && computerSelection === "Paper"){
-        console.log("You Lose! Paper beats Rock");
+        return winLoseMsg = "You Lose! Paper beats Rock";
     }
     else if(playerSelection === "Paper" && computerSelection === "Rock"){
-        console.log("You Win! Paper beats Rock");
+        return winLoseMsg = "You Win! Paper beats Rock";
     }
     else if(playerSelection === "Paper" && computerSelection === "Scissors"){
-        console.log("You Lose! Scissors beats Paper");
+        return winLoseMsg = "You Lose! Scissors beats Paper";
     }
     else if(playerSelection === "Scissors" && computerSelection === "Paper"){
-        console.log("You Win! Scissors beats Paper");
+        return winLoseMsg = "You Win! Scissors beats Paper";
     }
     else if(playerSelection === "Scissors" && computerSelection === "Rock"){
-        console.log("You Lose! Rock beats Scissors");
+        return winLoseMsg = "You Lose! Rock beats Scissors";
     }
     else{
-        console.log("Its a Tie!");
+        return winLoseMsg = "Its a Tie!";
     }
 
     return 0;
 }
-/*
-function game(){
 
-}*/
+function game(){
+    let count = 0;
+    let possibleWin1 = "You Win! Paper beats Rock";
+    let possibleWin2 = "You Win! Rock beats Scissors";
+    let possibleWin3 = "You Win! Scissors beats Paper";
+
+    for(i = 1; i <=5; ++i){
+        let playerChoice = prompt("Rock, Paper, or Scissors!");
+        let outCome = (playRound(playerChoice, computerChoice()));
+    
+        if(outCome == possibleWin1 || outCome == possibleWin2 || outCome == possibleWin3){
+            count++;
+        }
+        if(outCome === "Its a Tie!"){
+            i-=1;
+        }
+
+        console.log(outCome);
+    }
+
+    if(count >= 3){
+        console.log("You Win the game!");
+    }
+    else{
+        console.log("You Lost the game!");
+    }
+
+    return 0;
+}
 function capitalize(word1){
     word1 = word1.toLowerCase();
     let letter1 = word1.charAt(0);
@@ -53,5 +79,4 @@ function computerChoice(){
     return rpsChoice;
 }
 
-let playerChoice = prompt("Rock, Paper or Scissors!");
-console.log(computerChoice());
+game();
